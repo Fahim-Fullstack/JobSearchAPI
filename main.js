2c7b03b8018d6e49425267f7c3851d7e751e2b43
fahim Hasan <fhasan5@wgu.edu>
2024-06-06 19:00:38 -0500

    Job Item Displayed

--- main.js
+++ main.js
@@ -74,6 +74,30 @@ const submitHandler = (event) => {
 
       // render number of results
       numberEl.textContent = jobData.length;
+
+      // Render job items in search job list
+      jobData.forEach((jobData) => {
+        const newJobItemHTML = `
+            <li class="job-item">
+              <a class="job-item__link" href="${jobData.id}">
+                  <div class="job-item__badge">${jobData.badgeLetters}</div>
+                  <div class="job-item__middle">
+                      <h3 class="third-heading">${jobData.title}</h3>
+                      <p class="job-item__company">${jobData.company}</p>
+                      <div class="job-item__extras">
+                          <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i> ${jobData.duration}</p>
+                          <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i> ${jobData.salary}</p>
+                          <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${jobData.location}</p>
+                      </div>
+                  </div>
+                  <div class="job-item__right">
+                      <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
+                      <time class="job-item__time">${jobData.daysAgo}d</time>
+                  </div>
+              </a>
+          </li>`;
+        jobListSearchEl.insertAdjacentHTML("beforeend", newJobItemHTML);
+      });
     })
     .catch((error) => console.log(error));
 };
